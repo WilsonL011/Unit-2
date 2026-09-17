fe97a2dd3316468341ff87fcaf7bfda2f147a61a
WilsonL011 <wilsonl141@nycstudents.net>
2026-09-17 12:39:18 -0400

    Update Compilestuff.js

--- Compilestuff.js
+++ Compilestuff.js
@@ -9,9 +9,9 @@
 function fizzBuzz(num) {
 if (num %3 == 0)
     return "Fizz"; 
-else if (num %5 ==0)
+else if (num %5 == 0)
     return "Buzz";
-else if (num %3 ==0 && num%5 ==0)
+else if (num %3 == 0 && num%5 == 0)
     return "FizzBuzz";
 else 
  return String(num);
